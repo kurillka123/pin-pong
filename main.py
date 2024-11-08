329f76dd308fbea3420cdd3fd78bf08ba63a33f1
kurillka123 <145466273+kurillka123@users.noreply.github.com>
2024-11-08 19:33:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 import pygame
-'''
-мяч
-ракетка
-'''
+
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-class Racket:
-    def __init__(self, x: int, y: int, game):
-        '''
-        полнозкранный режим
-        как узнать размер полного экрана
-        помещать ракетки посередине вертикали
-        рассчитать размеры и координаты ракеток относительно экрана
-        '''
-        self.x = x
+class BALL:
+    width = 20
+    height = 20
+
+    def __init__(self, x:int, y:int, game):
+        self.x = x 
         self.y = y
-        self.width = 10
-        self.haight = 100
         self.color = WHITE
         self.speed = 5
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.haight)
+        self.rect = pygame.Rect(self.x, self.y, BALL.width, BALL.height)
+        self.game = game
+        
+    def move(self):
+        self.x += 1
+        self.y += 0
+        # TODO изменить координаты у self.rect
+
+    def render(self):
+        pygame.draw.rect(self.game.screen, self.color, self.rect, 0)
+
+    def update(self):
+        self.move()
+
+
+class Racket:
+    width = 40
+    height = 250
+    def __init__(self, x: int, y: int, game):
+        self.x = x 
+        self.y = y
+        self.color = WHITE
+        self.speed = 5
+        self.rect = pygame.Rect(self.x, self.y, Racket.width,  Racket.height)
         self.game = game
 
     def move(self, x: int, y: int):
@@ -33,19 +48,39 @@ class Racket:
         pygame.draw.rect(self.game.screen, self.color,self.rect, 0)
 
 
-
-
 class Game:
     def __init__(self, width: int, height: int) -> None:
         pygame.init()
-        display_info = pygame.display.Info()
-        self.window_width = display_info.current_w
-        self.window_height = display_info.current_h
+        display_info = pygame.display.Info() # экземпляр класса Info
+        self.window_width = display_info.current_w # атрибуты Info (ширина)
+        self.window_height = display_info.current_h # атрибуты Info (высота)
         self.screen = pygame.display.set_mode(
             (self.window_width, self.window_height)
         )
-        self.racket_left = Racket(50, 250, self)
-        self.racket_right = Racket(450, 250, self)
+        
+        # левая ракетка
+        self.racket_left = Racket(
+            self.window_width // 10, 
+            self.window_height // 2 - Racket.height // 2, 
+            self
+            )
+        
+        # правая ракетка
+        self.racket_right = Racket(
+            self.window_width // 10 * 9 - Racket.width,
+            self.window_height // 2 - Racket.height // 2,
+            self
+            )
+        
+        # мяч
+        x_ball = self.window_width // 2 - BALL.width // 2
+        y_ball = self.window_height // 2 - BALL.height // 2
+        self.ball = BALL(
+            x_ball,
+            y_ball, 
+            self
+            )
+        
         self.is_running = True
         self.main_loop()
 
@@ -57,25 +92,25 @@ class Game:
             рендер (отрисовка)
             ожидание FPS
             '''
-            self.handle_events()
-            self.update()
-            self.render()
+            self.handle_events() # вызов функции handle_events
+            self.update() # вызов функции update
+            self.render() # вызов функции render
         pygame.quit()
 
     def handle_events(self) -> None:
         '''обрабатывает события'''
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: 
                 self.is_running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    self.racket_right.move(0, -10)
+                    self.racket_right.move(0, -30)
                 elif event.key == pygame.K_DOWN:
-                    self.racket_right.move(0, 10)
+                    self.racket_right.move(0, 30)
                 elif event.key == pygame.K_w:
-                    self.racket_left.move(0, -10)
+                    self.racket_left.move(0, -30)
                 elif event.key == pygame.K_s:
-                    self.racket_left.move(0, 10)
+                    self.racket_left.move(0, 30)
                 elif event.key == pygame.K_ESCAPE:
                     self.is_running = False
                     
@@ -84,9 +119,13 @@ class Game:
 
     def render(self):
         '''отрисовывает обьекты на екране'''
+
+        self.ball.update()
+
         self.screen.fill(BLACK)
         self.racket_left.render()
         self.racket_right.render()
+        self.ball.render()
         pygame.display.flip()
 
 
