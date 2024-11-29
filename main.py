c0fae1ac1a453e1acdbeec8238308c69918aaf4e
kurillka123 <145466273+kurillka123@users.noreply.github.com>
2024-11-29 18:54:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
@@ -17,35 +18,47 @@ class Game:
         
         # левая ракетка
         x_left = self.window_width // 10
-        self.racket_left = Racket(
+        self.racket_left = RacketManual(
             x_left, 
-            pygame.K_w,
-            pygame.K_s,
-            self
-            )
-
-        # правая ракетка
-        x_right = self.window_width // 10 * 9 - Racket.width
-        self.racket_right = Racket(
-            x_right,
             pygame.K_UP,
             pygame.K_DOWN,
             self
-            )
+        )
+
+        # правая ракетка
+        x_right = self.window_width // 10 * 9 - Racket.width
+        self.racket_right = RacketAuto(
+            x_right,
+            self,
+        )
         
         # мяч
         self.ball = Ball(self)
 
         #левое табло 
-        self.score_left = Score(100, 100, self)
+        self.score_left_value = 0
+        self.score_left = Score(int(self.window_width * 0.25), 100, self)
 
         #правое табло
-        self.score_right = Score(600, 100, self)
+        self.score_right_value = 0
+        self.score_right = Score(int(self.window_width * 0.75), 100, self)
         
         self.keys_pressed = True
         self.is_running = True
         self.clock = pygame.time.Clock()
         self.main_loop()
+
+    def check_goal(self) -> None:
+        if self.ball.rect.right >  self.window_width:
+            self.score_left.value += 1
+            self.ball.goto_start()
+            self.racket_left.goto_start()
+            self.racket_right.goto_start()
+        elif self.ball.rect.left < 0:
+            self.score_right.value += 1
+            self.ball.goto_start()
+            self.racket_left.goto_start()
+            self.racket_right.goto_start()
 
     def main_loop(self) -> None:
         while self.is_running:
@@ -74,10 +87,12 @@ class Game:
                     
     def update(self) -> None:
         self.ball.update()
+        self.check_goal()
         self.racket_left.update()
         self.racket_right.update()
-        self.racket_left.update()
-        self.racket_right.update()
+        self.score_left.update()
+        self.score_right.update()
+
 
 
 
@@ -93,15 +108,16 @@ class Game:
 
 
 class Ball:
-    width = 20
-    height = 20
+    width = 15
+    height = 15
     speed = 3
 
     def __init__(self,game: Game) -> None:
         self.color = WHITE
         self.speed = Ball.speed
-        self.velocity_x = 1
-        self.velocity_y = 1
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.angle = 75 # углы в градусах
         self.rect = pygame.Rect(0, 0, Ball.width, Ball.height)
         self.game = game
         self.goto_start()
@@ -111,26 +127,25 @@ class Ball:
         self.rect.centery = self.game.window_height // 2
 
     def move(self) -> None:
+        self.velocity_x = math.cos(math.radians(self.angle - 90))
+        self.velocity_y = math.sin(math.radians(self.angle - 90))
         self.rect.x += self.velocity_x * self.speed
         self.rect.y += self.velocity_y * self.speed
 
     def collide_barders(self) -> None:
         '''столкновение с экраном'''
-        if self.rect.right >= self.game.window_width:
-            self.velocity_x *= -1
-        elif self.rect.left <= 0:
-            self.velocity_x *= -1
-        elif self.rect.top <= 0:
-            self.velocity_y *= -1
+        if self.rect.top <= 0:
+            self.angle *= -1
+            self.angle += 180
         elif self.rect.bottom >= self.game.window_height:
-            self.velocity_y *= -1
-
+            self.angle *= -1
+            self.angle += 180
     def collide_rackets(self):
         '''столкновение с ракетками'''
         if self.rect.colliderect(self.game.racket_left.rect):
-            self.velocity_x *= -1
+            self.angle *= -1
         elif self.rect.colliderect(self.game.racket_right.rect):
-            self.velocity_x *= -1
+            self.angle *= -1
             
 
     def render(self) -> None:
@@ -142,32 +157,26 @@ class Ball:
         self.collide_rackets()
 
 
-
 class Racket:
     width = 40
-    height = 250
-    speed = 3
+    height = 200
+    speed = 4
 
-    def __init__(self, center_x: int, key_up: int, key_down: int, game: Game):
+    def __init__(
+            self, 
+            center_x: int,
+            game: Game, 
+    ):
         self.center_x = center_x
         self.color = WHITE
         self.speed = Racket.speed
         self.rect = pygame.Rect(0, 0, Racket.width,  Racket.height)
-        self.key_up = key_up
-        self.key_down = key_down
         self.game = game
         self.goto_start()
 
     def goto_start(self):
         self.rect.centerx = self.center_x
         self.rect.centery = self.game.window_height // 2
-
-    def move(self):
-        '''двигает ракетку'''
-        if self.game.keys_pressed[self.key_down]:
-            self.rect.y += self.speed
-        elif self.game.keys_pressed[self.key_up]:
-            self.rect.y -= self.speed
 
     def collide_borders(self):
         if self.rect.bottom > self.game.window_height:
@@ -182,10 +191,39 @@ class Racket:
     def update(self):
         self.collide_borders()
         self.move()
+class RacketAuto(Racket):
+    def __init__(self, center_x, game):
+        super().__init__(center_x, game)
+        self.delay = 20
+        self.last_move = pygame.time.get_ticks()
+
+    def move(self):
+        if pygame.time.get_ticks() - self.last_move >= self.delay:
+            if self.game.ball.rect.centery < self.rect.centery:
+                self.rect.centery -= self.speed
+            elif self.game.ball.rect.centery > self.rect.centery:
+                self.rect.centery += self.speed
+            self.last_move = pygame.time.get_ticks()
+    
+
+class RacketManual(Racket):
+    def __init__(self, center_x, key_up, key_down, game):
+        super().__init__(center_x, game)
+        self.key_up = key_up
+        self.key_down = key_down
+
+    def move(self):
+        if self.game.keys_pressed[self.key_down]:
+            self.rect.y += self.speed
+        elif self.game.keys_pressed[self.key_up]:
+            self.rect.y -= self.speed
 
 class Score:
     '''табло для показа счета игрока'''
+
     def __init__(self, center_x: int, center_y: int, game: Game):
+        self.center_x = center_x
+        self.center_y = center_y
         self.value = 0
         self.color = WHITE
         self.size = 100
@@ -200,9 +238,10 @@ class Score:
         self.game.screen.blit(self.image, self.rect)
 
     def update(self):
-        pass
-
-
+        self.image = self.font.render(str(self.value), True, self.color)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.center_x
+        self.rect.centery = self.center_y
 
 
 Game()
